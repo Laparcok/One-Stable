141805645880ae3ffd8c1822d2b7b13bc33fc36c
Laparcok <105703266+Laparcok@users.noreply.github.com>
2022-05-31 16:02:00 +0700

    Update gcbot.js

--- plugins/gcbot.js
+++ plugins/gcbot.js
@@ -6,11 +6,11 @@ let handler = async (m, { conn, text }) => {
 _*Grup Official*_
 ━━━━━━━━━━━━━━━
 ┏━━━━━━━━━━━━━ꕥ
-┃ *_Grup Utama_*  : https://chat.whatsapp.com/GfwblXDHAqF4iA27wk8Gf4 => Ramein
-┃ *_Grup Kedua_*  : https://chat.whatsapp.com/EheAWPrQMhV25xW0N7l7WD
-┃ *_Grup Ketiga_* : https://chat.whatsapp.com/GzDQlgoUamS5Ok4EOcYRRB
-┃ *_Grup Keempat_* : https://chat.whatsapp.com/ESeBcdKcHFm7BnyNiSZPvi
-┃ *_Grup Kelima_* : https://chat.whatsapp.com/HhHDKEpxtyC8B1cj3KGhlS
+┃ *_Grup Utama_*  : https://tinyurl.com/y5ck3k4u => 𝐑𝐚𝐦𝐞𝐢𝐧 𝐛𝐚𝐧𝐡:𝐯 𓂺
+┃ *_Grup Kedua_*  : 𝐍𝐨𝐭 𝐟𝐨𝐮𝐧𝐝
+┃ *_Grup Ketiga_* : 𝐍𝐨𝐭 𝐟𝐨𝐮𝐧𝐝
+┃ *_Grup Keempat_* : 𝐍𝐨𝐭 𝐟𝐨𝐨𝐮𝐧𝐝
+┃ *_Grup Kelima_* : 𝐍𝐨𝐭 𝐅𝐨𝐮𝐧𝐝
 ┗━━━━━━━━━━━━━ꕥ
 `.trim()
 conn.send2ButtonLoc(m.chat, await (await fetch(fla + 'One Groups')).buffer(), ext, 'One', 'SIAP BANG🗿', 'Iya', 'Owner', '.owner', m)
